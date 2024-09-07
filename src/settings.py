6c1253f52ada3cb67bd187f86e22ed0724d154ff
Egor <ega.nik5674@gmail.com>
2024-09-07 14:50:30 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,12 @@ SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 
 RANGE_NAME = os.getenv("RANGE_NAME")
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+BOT_PASSWORD = os.getenv("BOT_PASSWORD")
+
 CREDENTIALS_PATH = Path(__file__).parent.parent.joinpath("credentials.json")
 
 DATAPATH = Path(__file__).parent.parent.joinpath("data", "data.json")
+
+RESULT_PATH = Path(__file__).parent.parent.joinpath("data", "result.json")
